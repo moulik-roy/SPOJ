4f0af487a1209c3bebbf298686f5c7568ac8d0c3
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2019-06-11 14:56:46 +0530

    Add files via upload

---
+++ GCPC11J.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+vector <int> bfs(vector <int> tree[], int n, int s){
+	int i;
+	vector <int> dist(n, 0), visited(n, 0);
+	queue <int> q;
+	q.push(s);
+	while(!q.empty()){
+		s=q.front();
+		visited[s]=1;
+		for(i=0; i<tree[s].size(); i++){
+			if(!visited[tree[s][i]]){
+				dist[tree[s][i]]=dist[s]+1;
+				q.push(tree[s][i]);
+			}
+		}
+		q.pop();
+	}
+	return dist;
+}
+
+int main(){
+	int T, N, a, b, i;
+	cin>>T;
+	while(T--){
+		cin>>N;
+		vector <int> tree[N], dist;
+		for(i=1; i<N; i++){
+			cin>>a>>b;
+			tree[a].push_back(b);
+			tree[b].push_back(a);
+		}
+		dist=bfs(tree, N, 0);
+		i=max_element(dist.begin(), dist.end())-dist.begin();
+		dist=bfs(tree, N, i);
+		cout<<(*max_element(dist.begin(), dist.end())+1)/2<<"\n";
+	}
+	return 0;
+}
