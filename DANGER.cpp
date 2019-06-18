cde05eabbdb4b80d62ef205f86b98db5dbe8e04b
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2019-06-18 17:42:40 +0530

    Add files via upload

---
+++ DANGER.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+	string s;
+	int n, x, y, z, p, winner;
+	while(cin>>s && s.compare("00e0")!=0){
+		x=s[0]-'0';
+		y=s[1]-'0';
+		z=s[3]-'0';
+		n=(10*x)+y;
+		while(z--)
+			n=n*10;
+		p=(int)log2(n);
+		winner=2*(n-(1<<p))+1;
+		cout<<winner<<"\n";
+	}
+	return 0;
+}
