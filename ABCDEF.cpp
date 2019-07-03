a47eb066abcf053702928472b74716501314811d
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2019-07-03 18:25:49 +0530

    Update ABCDEF.cpp

--- ABCDEF.cpp
+++ ABCDEF.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int main(){
-	long int n, i, j, k, c;
+	long int N, i, j, k, count;
 	vector <long int> v1, v2;
-	cin>>n;
-	long int a[n];
-	for(i=0; i<n; i++)
+	cin>>N;
+	long int a[N];
+	for(i=0; i<N; i++)
 		cin>>a[i];
-	for(i=0; i<n; i++){
-		for(j=0; j<n; j++){
-			for(k=0; k<n; k++){
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			for(k=0; k<N; k++){
 				v1.push_back(a[i]*a[j]+a[k]);
 				if(a[k]!=0)
 					v2.push_back((a[i]+a[j])*a[k]);
@@ -21,8 +21,8 @@ int main(){
 	}
 	sort(v1.begin(), v1.end());
 	sort(v2.begin(), v2.end());
-	for(i=0, c=0; i<v1.size(); i++)
-		c=c+(upper_bound(v2.begin(), v2.end(), v1[i])-lower_bound(v2.begin(), v2.end(), v1[i]));
-	cout<<c<<"\n";
+	for(i=0, count=0; i<v1.size(); i++)
+		count+=(upper_bound(v2.begin(), v2.end(), v1[i])-lower_bound(v2.begin(), v2.end(), v1[i]));
+	cout<<count<<"\n";
 	return 0;
 }
