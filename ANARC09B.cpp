83062d7706c34708489e45401849041fe66dff07
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2019-07-08 19:28:06 +0530

    Update ANARC09B.cpp

--- ANARC09B.cpp
+++ ANARC09B.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main(){
     long int W, H, min_tiles;
     while(cin>>W>>H && W+H!=0){
-    	if(W%H==0)
-    		min_tiles=W/H;
-    	else if(H%W==0)
-    		min_tiles=H/W;
+        if(W%H==0)
+            min_tiles=W/H;
+        else if(H%W==0)
+            min_tiles=H/W;
         else
             min_tiles=W*H;
         cout<<min_tiles<<"\n";
