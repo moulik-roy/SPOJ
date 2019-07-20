2856afdf7117f2ab839e313df7bcaa18ab9482d7
Moulik Roy <37454620+moulik77@users.noreply.github.com>
2019-07-21 00:18:08 +0530

    Add files via upload

--- TRICOUNT.cpp
+++ TRICOUNT.cpp
@@ -6,10 +6,7 @@ int main(){
 	cin>>T;
 	while(T--){
 		cin>>N;
-		if(N%2==0)
-			count=(N*(N+2)*(2*N+1))/8;
-		else
-			count=(N*(N+2)*(2*N+1)-1)/8;
+		count=(N*(N+2)*(2*N+1))/8;
 		cout<<count<<"\n";
 	}
 	return 0;
