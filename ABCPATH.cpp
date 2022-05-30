0e5be8a566bc9540eb26940489e75627f64b6af5
Moulik Roy <37454620+moulik-roy@users.noreply.github.com>
2022-05-31 02:14:52 +0530

    Add files via upload

--- ABCPATH.cpp
+++ ABCPATH.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <string>
-#include <cstring>
+#include <vector>
 using namespace std;
 
-int visited[52][52], dx[8]={0, 1, 1, 1, 0, -1, -1, -1}, dy[8]={1, 1, 0, -1, -1, -1, 0, 1};
+int dx[8]={0, 1, 1, 1, 0, -1, -1, -1}, dy[8]={1, 1, 0, -1, -1, -1, 0, 1};
 
-void dfs(string s[], int H, int W, int i, int j, int length, int &max_length){
+int dfs(string s[], int H, int W, int i, int j, vector <vector<int>> &dp){
 	int k, x, y;
-	visited[i][j]=1;
-	if(max_length<length)
-		max_length=length;
+	if(dp[i][j]!=-1)
+		return dp[i][j];
+	dp[i][j]=1;
 	for(k=0; k<8; k++){
 		x=i+dx[k];
 		y=j+dy[k];
-		if(x>=0 && x<H && y>=0 && y<W && s[x][y]==s[i][j]+1 && !visited[x][y])
-			dfs(s, H, W, x, y, length+1, max_length);
+		if(x>=0 && x<H && y>=0 && y<W && s[x][y]==s[i][j]+1)
+			dp[i][j]=max(dp[i][j], 1+dfs(s, H, W, x, y, dp));
 	}
+	return dp[i][j];
 }
 
 int main(){
@@ -23,13 +24,13 @@ int main(){
 	C=1;
 	while(cin>>H>>W && H+W!=0){
 		string grid[H];
+		vector <vector<int>> dp(H, vector<int>(W, -1));
 		for(i=0; i<H; i++)
 			cin>>grid[i];
-		memset(visited, 0, sizeof(visited));
 		for(i=0, max_length=0; i<H; i++){
 			for(j=0; j<W; j++){
-				if(grid[i][j]=='A' && !visited[i][j])
-					dfs(grid, H, W, i, j, 1, max_length);			
+				if(grid[i][j]=='A')
+					max_length=max(max_length, dfs(grid, H, W, i, j, dp));
 			}
 		}
 		cout<<"Case "<<C++<<": "<<max_length<<"\n";
