67ada28e609a32117d784541bb25594acfd057db
Moulik Roy <37454620+moulik-roy@users.noreply.github.com>
2022-06-12 01:19:43 +0530

    Add files via upload

--- AE00.cpp
+++ AE00.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main(){
-	int N, i, j, r, count;
-	cin>>N;
-	for(i=2, count=1; i<=N; i++){
-		r=sqrt(i);
-		for(j=1; j<=r; j++){
-			if(i%j==0)
-				count++;
-		}
-	}
-	cout<<count<<"\n";
-	return 0;
+    int N, i, j, count;
+    cin>>N;
+    for(i=2, count=1; i<=N; i++){
+        for(j=1; j*j<=i; j++){
+            if(i%j==0)
+                count++;
+        }
+    }
+    cout<<count<<"\n";
+    return 0;
 }
