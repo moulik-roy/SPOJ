13432642dc5f4d1d7dc06c197f0f66800b00f1e8
Moulik Roy <37454620+moulik-roy@users.noreply.github.com>
2022-06-27 21:41:32 +0530

    Add files via upload

---
+++ NHAY.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> LPS(string &s){
+    int i, j;
+    vector <int> lps(s.length(), 0);
+    for(i=1, j=0; i<s.length(); i++){
+        while(j>0 && s[i]!=s[j])
+            j=lps[j-1];
+        if(s[i]==s[j])
+            j++;
+        lps[i]=j;
+    }
+    return lps;
+}
+
+int main(){
+    int length, i, j;
+    string needle, haystack;
+    while(cin>>length){
+        cin>>needle>>haystack;
+        vector <int> lps=LPS(needle);
+        i=0;    j=0;
+       for(i=0; i<haystack.length(); i++){
+            while(j>0 && haystack[i]!=needle[j])
+                j=lps[j-1];
+            if(haystack[i]==needle[j])
+                j++;
+            if(j==length){
+                cout<<i-length+1<<"\n";
+                j=lps[j-1];
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
